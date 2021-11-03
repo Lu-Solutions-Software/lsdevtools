dd7c87f10d6dbea2f831e1ef0ef5a0069bcd6f11
Ludwig Schindler <ludwigntd@gmail.com>
2021-11-03 22:56:35 +0100

    Beispiel UI für das Inkrement Tool

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -12,8 +12,101 @@ class SampleItemDetailsView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Item Details'),
       ),
-      body: const Center(
-        child: Text('More Information Here'),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const Spacer(),
+              Expanded(
+                child: TextFormField(
+                  initialValue: "%X%",
+                  decoration: const InputDecoration(
+                    labelText: "Variable",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                child: TextFormField(
+                  initialValue: "1",
+                  decoration: const InputDecoration(
+                    labelText: "Startwert",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                child: TextFormField(
+                  initialValue: "30",
+                  decoration: const InputDecoration(
+                    labelText: "Endwert",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                child: TextFormField(
+                  initialValue: "1",
+                  decoration: const InputDecoration(
+                    labelText: "Inkrement",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Expanded(child: CheckboxListTile(value: true, onChanged: (_) {},  title: const Text("Auto copy"),)),
+              Spacer(),
+              // ElevatedButton(onPressed: () {}, child: const Text("Berechnen")),
+              // const Spacer(),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Divider(),
+          Row(
+            children: [
+              const Spacer(),
+              Flexible(
+                flex: 1,
+                  child: TextFormField(
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: const InputDecoration(
+                  labelText: "Roheingabe",
+                ),
+              )),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.check)),
+          const Spacer(),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const Spacer(),
+              Flexible(
+                flex: 1,
+                child: TextFormField(
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      labelText: "Ergebnis",
+                    )),
+              ),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.copy)),
+              const Spacer(),
+            ],
+          ),
+        ],
       ),
     );
   }
